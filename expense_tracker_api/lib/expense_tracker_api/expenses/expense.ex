4f2e50430469f208acbf2dc589c71f96b9e3dea2
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2025-09-22 17:03:59 -0600

    feat: added currency field in expense model

--- expense_tracker_api/lib/expense_tracker_api/expenses/expense.ex
+++ expense_tracker_api/lib/expense_tracker_api/expenses/expense.ex
@@ -9,6 +9,7 @@ defmodule ExpenseTrackerApi.Expenses.Expense do
     field :description, :string
     field :category, Ecto.Enum, values: @categories
     field :date, :date
+    field :currency, :string
 
     belongs_to :user, ExpenseTrackerApi.Accounts.User
 
@@ -18,13 +19,15 @@ defmodule ExpenseTrackerApi.Expenses.Expense do
   @doc false
   def changeset(expense, attrs) do
     expense
-    |> cast(attrs, [:amount, :description, :category, :date, :user_id])
+    |> cast(attrs, [:amount, :description, :category, :date, :currency, :user_id])
     |> put_default_date()
     |> put_default_category()
-    |> validate_required([:amount, :description, :category, :user_id])
+    |> put_default_currency()
+    |> validate_required([:amount, :description, :category, :currency, :user_id])
     |> validate_number(:amount, greater_than: 0)
     |> validate_length(:description, min: 1, max: 255)
     |> validate_inclusion(:category, @categories)
+    |> validate_currency_format()
   end
 
   defp put_default_date(%Ecto.Changeset{changes: %{date: _}} = changeset), do: changeset
@@ -35,6 +38,16 @@ defmodule ExpenseTrackerApi.Expenses.Expense do
   defp put_default_category(%Ecto.Changeset{changes: %{category: _}} = changeset), do: changeset
   defp put_default_category(changeset) do
     put_change(changeset, :category, :others)
+  end
+
+  defp put_default_currency(%Ecto.Changeset{changes: %{currency: currency}} = changeset) when currency != nil, do: changeset
+  defp put_default_currency(changeset) do
+    put_change(changeset, :currency, "USD")
+  end
+
+  defp validate_currency_format(changeset) do
+    validate_format(changeset, :currency, ~r/^[A-Z]{3}$/,
+      message: "must be a valid 3-letter ISO currency code")
   end
 
   def categories, do: @categories
